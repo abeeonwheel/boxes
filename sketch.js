26695d8760006c85d02e88935d97fe9a23a16da7
abeeonwheel <51782616+abeeonwheel@users.noreply.github.com>
2021-06-27 12:42:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3= new Box(800,320,70,70)
+    box4= new Box(720,220,70,70)
+    box5= new Box(760,300,70,70)
+    box6= new Box(600,250,70,70)
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+}
